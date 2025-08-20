958edbf3d0d5daffa50504c86db122c0771a1eb6
Clive Bluston <clive.bluston@gmail.com>
2025-08-20 14:39:27 +0300

    Production flow creates tag in github.

--- src/authService.ts
+++ src/authService.ts
@@ -5,7 +5,7 @@ import { cookieUtils } from "./cookieUtils.js";
 // Authentication state
 export let UserEmailValue = "";
 export let SignedIn = false;
-export let UIVersion = "1.13";
+export let UIVersion = "1.14";
 export let ServerVersion = "";
 
 // Customer management
