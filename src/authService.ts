dbae2fff475a44ec6783d368aee451076602e096
Clive Bluston <clive.bluston@gmail.com>
2025-09-25 18:26:20 +0300

    Dont clear messages on tax calculation.

--- src/authService.ts
+++ src/authService.ts
@@ -6,7 +6,7 @@ import { cookieUtils } from "./cookieUtils.js";
 export let UserEmailValue = "";
 export let UserRole = "";
 export let SignedIn = false;
-export let UIVersion = "1.33";
+export let UIVersion = "1.34";
 export let ServerVersion = "";
 
 // Customer management
