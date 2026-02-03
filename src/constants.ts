fd842243fd57a45604e942018e34889ea7d12749
Clive Bluston <clive.bluston@gmail.com>
2026-02-03 17:24:18 +0200

    Added support for tax free income.

--- src/constants.ts
+++ src/constants.ts
@@ -107,6 +107,7 @@ const friendlyNames = {
   LossesTransferredFromPreviousYear: "הפסדים שהועברו משנה קודמת",
   TaxPaidAbroadThatIsAllowedAsADeductionOrCredit: "מס ששולם בחו״ל הותׁר בניכוי או בזיכוי",
   TaxPaidAbroadThatIsNotAllowedAsADeductionOrCredit: "מס ששולם בחו״ל שלא הותׁר בניכוי או בזיכוי",
+  TaxFreeIncome_332: "הכנסה פטורה ממס",
   registeredTaxpayerBoolean: "בן/בת זוג רשום",
   birthDate: "תאריך לידה",
   children: "ילדים",
