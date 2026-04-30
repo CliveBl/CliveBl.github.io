5fa84a1d8119f737697d7d9e9194018a1ddb0d86
Clive Bluston <clive.bluston@gmail.com>
2026-04-30 19:10:46 +0300

    Added document types for Prisa, mas and tax preparation fee receipt.

--- src/authService.ts
+++ src/authService.ts
@@ -6,7 +6,7 @@ import { cookieUtils } from "./cookieUtils.js";
 export let UserEmailValue = "";
 export let UserRole = "";
 export let SignedIn = false;
-export let UIVersion = "1.56";
+export let UIVersion = "1.57";
 export let ServerVersion = "";
 
 // Customer management
