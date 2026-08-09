0f963033021ab694f7ca275cc4d8cc0bea38a67d
Clive Bluston <clive.bluston@gmail.com>
2026-08-09 20:44:00 +0300

    Enable caching of getBasicInfo

--- src/authService.ts
+++ src/authService.ts
@@ -6,7 +6,7 @@ import { cookieUtils } from "./cookieUtils.js";
 export let UserEmailValue = "";
 export let UserRole = "";
 export let SignedIn = false;
-export const UIVersion = "1.72";
+export const UIVersion = "1.73";
 export let ServerVersion = "";
 
 export const WarningPopupMessage = 'אתר זה זמין ללא תשלום במטרה לסייע לאנשים המעוניינים להכין את הדו״ח השנתי שלהם למס הכנסה בעצמם. איננו מייצגים אתכם מול רשויות המס. אנא קראו בעיון את התנאים וההגבלות לפני המשך השימוש.<p class="general-warning-text">שים לב: האתר נמצא בשלב בדיקות בטא  .</p>'
@@ -676,7 +676,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   try {
     UserEmailValue = "";
 
-    const basicInfo = await getBasicInfo(true);
+    const basicInfo = await getBasicInfo(false);
     if (basicInfo) {
       ServerVersion = basicInfo.productVersion;
       UserEmailValue = basicInfo.userEmail;
